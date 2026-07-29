e2b32c2585dbd695818a42b0933480148b56ca08
Anand Jaiswal <aj569971@gmail.com>
2026-07-29 16:54:43 +0530

    Test Render deployment

--- intellmeet-backend/server.js
+++ intellmeet-backend/server.js
@@ -1,33 +1,17 @@
 const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-
-dotenv.config();
-
-const connectDB = require("./config/db");
-
-const meetingRoutes = require("./routes/meetingRoutes");
-
-connectDB();
 
 const app = express();
 
-app.use(cors());
-
-app.use(express.json());
-
-app.use("/api/meetings", meetingRoutes);
-
 app.get("/", (req, res) => {
+  res.send("Hello from Render!");
+});
 
-    res.send("🚀 IntellMeet Backend Running");
-
+app.get("/test", (req, res) => {
+  res.json({ success: true });
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
-
+  console.log(`Server running on port ${PORT}`);
 });
